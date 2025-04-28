f77fa89160d4cc2a08433dfd8afc835ecd03daee
filipe@discente.ufcat.edu.br <filipe.pereira@discente.ufcat.edu.br>
2025-04-28 17:00:32 -0300

    Organização variáveis

--- my_example.py
+++ my_example.py
@@ -28,10 +28,11 @@ def calcular_sigma_max(f_z: float, m_x: float, m_y: float, h_x: float, h_y: floa
     Esta função determina a tensão máxima e a tensão mínima na fundação rasa do tipo sapata
 
     Args
-    f_z: carregamento na direção (kN)
+    f_z: carregamento na direção z (kN)
 
     Returns
-    sigma_max: 
+    sigma_max: Tensão máxima suportada pela sapata (kN/m2)
+    sigma_min: Tensão minima suportada pela sapata (kN/m2)
 
     """
     
@@ -43,13 +44,22 @@ def calcular_sigma_max(f_z: float, m_x: float, m_y: float, h_x: float, h_y: floa
     
     return (sigma_fz) * (1 + aux_mx + aux_my), (sigma_fz) * (1 - aux_mx - aux_my)
 
-
 def volume_fundacao(h_x, h_y, h_z):
-    
+    '''
+    Esta função cálcula o volume da sapata
+
+    args:
+        h_x (float): dimensão hx(m)
+        h_y (float): dimensão hy(m)
+        h_z (float): dimensão hz(m)
+
+    Returns:
+        volume (float): volume da sapata (m3)
+    '''
+
     h_z= 0.6
 
     return h_x * h_y * h_z
-
 
 def cargas_combinacoes(cargas: list) -> list:
     """
@@ -106,12 +116,25 @@ def tensao_adm_solo(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 #Restricao de tensão
-def restricao_tensao(dados_fundacao,h_x, h_y, calcular_sigma_max):
+def restricao_tensao(h_x: float, h_y: float, none_variable, calcular_sigma_max):
+    '''
+   Esta função verifica se a tensão máxima é superior ou não a tensão admissível.
+   
+   args:
+       h_x (float): dimensão em x (m)
+       h_y (float): dimensão em y (m) 
+       comb (list): contendo as combinações de carregamento
+       sigma_lim (float): tensão admissível (kPa)
+
+   Returns:
+        result (float): valor da penalidade (admensional)
+    '''
+
     # Variáveis de projeto
-    h_x = dimensoes_sapata_a #Aqui quero pegar o resultado que cada iteração do código, mas para isso preciso de um valor inicial
-    h_y = dimensoes_sapata_b
-    comb = dados_fundacao['combinações']
-    sigma_lim = dados_fundacao['sigma_adm (kPa)']
+    h_x = x[0]
+    h_y = x[1]
+    comb = none_variable['combinações'] #é preciso fazer o for duplo
+    sigma_lim = none_variable['sigma_adm (kPa)']
 
     # Verificação da restrição
     result = []
@@ -128,36 +151,55 @@ def restricao_tensao(dados_fundacao,h_x, h_y, calcular_sigma_max):
     return result
 
 #Restiricao geometrica - Balanço
-def restricao_geometrica(A, B, a, b):
+def restricao_geometrica(A: float, B: float, a: float, b: float):
+    '''
+    Esta função cálcula o balanço da sapata e verifica se esta de acordo as dimensões permitidadas
 
-    # Definir as dimensões da sapata
-    A = dimensoes_sapata_a # dimensão hx(m)
-    B = dimensoes_sapata_b # dimensão hy(m)
+    args:
+        A (float): dimensões em x (m)
+        B (float): dimensões em y (m)  
+        a (float): dimensões dos pilares em x (m)
+        b (float): dimensões dos pilares em y (m)
+        none_variable (dicionário): dicionário com as dimensões dos pilares (m)
+
+    Returns:
+        result (float): valor da penalidade (admensional)
+    '''
+    # Dimensões da sapata
+    A = x[0]  # dimensão hx(m)
+    B = x[1] # dimensão hy(m)
     
-    # Buscar as dimensões dos pilares
-    a = dados_fundacao['ap']
-    b = dados_fundacao['bp']
+    # Dimensões dos pilares
+    a = none_variable['dados estrutura']['ap (m)']
+    b = none_variable['dados estrutura']['bp (m)']
 
-    # Para calcular o balanço na direção X
+    # Balanço na direção X
     Ca = (A - a)/2
 
-    # Para calcular o balanço na direção Y
+    # Balanço na direção Y
     Cb = (B - b)/2
 
-    # Verificaçao todas as linhas satisfazem as restrições
-    if ((Ca/(0.60 - a)/2 - 1 >= 0) & (Cb/(0.60 - b)/2 - 1 >= 0)):
+    # Verificaçao se satisfazem as restrições
+    if (Ca/(0.60 - a)/2 - 1 >= 0) and (Cb/(0.60 - b)/2 - 1 >= 0):
         return 0  # Restrição satisfeita
     else:
         return 1 # Restrição não satisfeita
     
 #Definição kx e ky para restrição de punção de acordo com NBR ....
 ## para kx
-def interpolar_kx(dados_fundacao):
-    
-    a = dados_fundacao ['ap']
-    b = dados_fundacao ['bp']
+def interpolar_kx(a: float, b: float):
+    '''
+    Esta função interpola o valor de Kx com base nas dimensões do pilar, necessário para a restrição de punção
+
+    args:
+        none_variable (dicionário): dicionário com as dimensões 'a' e 'b' da sapata
+    returns:
+        kx_interpolado (float): valor de Kx interpolado    
+    '''
+    a = none_variable ['dados estrutura'] ['ap (m)']
+    b = none_variable ['dados estrutura'] ['bp (m)']
     b_a = a / b
-    # Tabela de valores conhecidos
+    # Tabela de valores conhecidos da NBR...
     tabela = {
         0.5: 0.45,
         1.0: 0.60,
@@ -188,14 +230,23 @@ def interpolar_kx(dados_fundacao):
             # Interpolação linear
             kx_interpolado = y0 + (y1 - y0) * ((b_a - x0) / (x1 - x0))
             return round(kx_interpolado, 4)
-##para ky
-def interpolar_ky(dados_fundacao):
 
-    a = dados_fundacao ['ap']
-    b = dados_fundacao ['bp']
+##para ky
+def interpolar_ky(a: float, b: float):
+    '''
+    Esta função interpola o valor de Ky com base nas dimensões do pilar, para restrição de punção
+
+    args:
+        none_variable (dicionário): dicionário com as dimensões 'a' e 'b' da sapata
+    returns:
+        ky_interpolado (float): valor de Ky interpolado
+    '''
+
+    a = none_variable ['dados estrutura'] ['ap (m)']
+    b = none_variable ['dados estrutura'] ['bp (m)']
     a_b = a / b
 
-    # Tabela de valores conhecidos
+    # Tabela de valores conhecidos da NBR...
     tabela = {
         0.5: 0.45,
         1.0: 0.60,
@@ -228,23 +279,42 @@ def interpolar_ky(dados_fundacao):
             return round(ky_interpolado, 4)
 
 #restrição de punção para pilar central
-def restricao_puncao(dados_fundacao, A, B, MX, MY, Fz, rec):
-    #variáveis
-    a = dados_fundacao ['ap']
-    b = dados_fundacao ['bp']
-    A= dimensoes_sapata_a # preciso chamar as dimensões da sapata que serão utilizadas em cada interação!!
-    B= dimensoes_sapata_b # preciso chamar as dimensões da sapata que serão utilizadas em cada interação!!
-    d = 0.2 - rec #Altura util da sapata d = h - rec, 0,04 é um valor qualquer que deve ser especificado , quando tiver formato inclinado, d pode assumir valores diferentes em C e C'
+def restricao_puncao(A: float, B: float, a: float, b: float, h_z: 0.6, rec: float):
+    '''
+    Esta função verifica a se há esforço de punção na sapata e atibui ou não uma penalidade 
+    (Não é válida para fundações de canto, ou seja com o pilar na latera da sapata) 
+
+    Args:
+        x (list): Dimensões A e B (h_x e h_y) da sapata (m)
+        none_variable (dict): Dicionário com as dimensões do pilar  da sapata (m)
+        kx e ky (float): Coeficientes de restrição de punção (admensional)
+        ro (float): Densidade do concreto (kg/m3)
+        fcd (float): Resistencia a copressão do concreto (..)
+        ...... : combinação dos esforços
+        .....
+    returns:
+        g1 ou g2, (float): Maior valor da penalidades para linha critica 1 ou 2, da punção (admensional)
+    
+    '''
+    
+    #DImensões da sapata
+    A= x[0] 
+    B= x[1] 
+    #Dimensões do pilar
+    a = none_variable ['dados estrutras']['ap (m)']
+    b = none_variable ['dados estrutras']['bp (m)']
+    rec = none_variable ['rec (m)']
+    d = h_z - rec #Altura util da sapata d = h - rec, 0,04 é um valor qualquer que deve ser especificado , quando tiver formato inclinado, d pode assumir valores diferentes em C e C'
     MX = dados_fundacao[1]['combinações'] #MX = combinations [1]
     MY = dados_fundacao[2]['combinções']  
     Fz = dados_fundacao[0]['combinações']
 
     #coeficientes
     sigma_cp = 0 # tensão a mais devido a efeitos da protensão
-    ro = 0.01 # rô também vira restrição pois não pode ser maior que 2% !!!!!
+    ro = 0.01 # taxa geométrica de armadura de flexão aderente <0,02 (NBR6118-2023)
     ke = 1 + math.sqrt(20 / (d * 100))
-    kx = interpolar_kx(a, b)
-    ky = interpolar_ky(a, b)
+    kx = interpolar_kx(none_variable)
+    ky = interpolar_ky(none_variable)
 
     # calculando Fcd
     fcd = fck / 1.4
@@ -315,14 +385,22 @@ def obj_ic_fundacoes(x, none_variable):
     
     print(vol)
 
-    g_c1 = []
+    g_c = []
+    
+    #for index, row in df.iterrows():
+        #f_z = row['Fz-c1']
+       # m_x = row['Mx-c1']
+        #m_y = row['My-c1']
+        #g = calcular_sigma_max(f_z, m_x, m_y, h_x, h_y)
+        #g_c1.append(g)
     for index, row in df.iterrows():
-        f_z = row['Fz-c1']
-        m_x = row['Mx-c1']
-        m_y = row['My-c1']
-        g = calcular_sigma_max(f_z: float, m_x: float, m_y, h_x, h_y)
-        g_c1.append(g)
-
+        for i in range(1, 4): #alterar para 1, 11 quando for usar a tabela correta
+            f_z = row[f'Fz-c{i}']
+            m_x = row[f'Mx-c{i}']
+            m_y = row[f'My-c{i}']
+            g = calcular_sigma_max (f_z, m_x, m_y, h_x, h_y,)
+            g_c.append(g)
+    
     # # Determina o volume do elemento de fundação
     
 
@@ -341,7 +419,7 @@ def obj_ic_fundacoes(x, none_variable):
     
 
 
-def data_comb(df: pd.DataFrame) -> list:
+def data_comb(df: pd.DataFrame,) -> list:
     """
     Gera combinações 3 a 3 das colunas de uma planilha com header duplo,
     identificando as combinações automaticamente a partir do DataFrame,
@@ -402,12 +480,14 @@ if __name__ == 'IC_Filipe':
 
 if __name__== '__main__':
     df = pd.read_excel("teste_wand.xlsx")
-    print(df)
+    #print(df)
     a = 0.6
     b = 0.6
     x = [a, b]
-    none_variable = {'dados estrutura': df, 'h_z (m)': 0.6}
+    none_variable = {'dados estrutura': df, 'h_z (m)': 0.6, 'rec (m)': 0.025, 'fck (MPa)': 25 }
     ofi = obj_ic_fundacoes(x, none_variable)
-    # print(ofi)
+    #print("resultado ofi: ",ofi)
+    teste = restricao_geometrica(A, B, a, b)
+    print(teste)
 else :
     print("nada")
